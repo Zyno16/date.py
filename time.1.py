7ce4b0730b1b68f41f4f149ded70dd044cbb7abc
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-03 15:47:02 +0000

    Add files via upload

---
+++ time.1.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+
+
+dt =datetime.date(1990,4,6) #display date
+md = datetime.time(14,20,15,1245) #display time
+print(dt ,md)
+
+
+
+#dt = datetime.datetime.now().date() #today day
+#print(dt)
+
+#dt =datetime.datetime.now() today date and time
+#print(dt)
